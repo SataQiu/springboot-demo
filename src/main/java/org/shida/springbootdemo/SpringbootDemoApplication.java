1d9307666ebd2e3446de448b0ffbf3f164d24fc7
SataQiu <qiushida@beyondcent.com>
2019-04-30 10:57:20 +0800

    fmt

--- src/main/java/org/shida/springbootdemo/SpringbootDemoApplication.java
+++ src/main/java/org/shida/springbootdemo/SpringbootDemoApplication.java
@@ -2,7 +2,11 @@ package org.shida.springbootdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @SpringBootApplication
 public class SpringbootDemoApplication {
 
@@ -10,4 +14,9 @@ public class SpringbootDemoApplication {
 		SpringApplication.run(SpringbootDemoApplication.class, args);
 	}
 
+	@RequestMapping("/")
+	@ResponseBody
+	public String home() {
+		return "hello world";
+	}
 }
